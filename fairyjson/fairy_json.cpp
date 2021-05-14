035083b5f462efbc4b9b2005156f89896c682b6e
yubinCloud <yubin_SkyWalker@yeah.net>
2021-05-14 09:29:48 +0800

    1. 完成解析 true 和 false

--- fairyjson/fairy_json.cpp
+++ fairyjson/fairy_json.cpp
@@ -44,6 +44,39 @@ namespace fairy {
     }
 
     /**
+     * 解析 true
+     * true == "true"
+     * @param c
+     * @param v
+     * @return
+     */
+    static JsonParseStatus parseTrue(ParseContext* c, FairyValue* v) {
+        EXPECT(c, 't');
+        if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e') {
+            return JsonParseStatus::PARSE_INVALID_VALUE;
+        }
+        c->json += 3;
+        v->type = JsonFieldType::J_TRUE;
+        return JsonParseStatus::PARSE_OK;
+    }
+
+    /**
+     * 解析 false
+     * @param c
+     * @param v
+     * @return
+     */
+    static JsonParseStatus parseFalse(ParseContext* c, FairyValue* v) {
+        EXPECT(c, 'f');
+        if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e') {
+            return JsonParseStatus::PARSE_INVALID_VALUE;
+        }
+        c->json += 4;
+        v->type = JsonFieldType::J_FALSE;
+        return JsonParseStatus::PARSE_OK;
+    }
+
+    /**
      * 解析 json 值
      * @param c
      * @param v
@@ -53,6 +86,8 @@ namespace fairy {
         switch (*c->json)
         {
             case 'n':   return parseNull(c, v);
+            case 't':   return parseTrue(c, v);
+            case 'f':   return parseFalse(c, v);
             case '\0':  return JsonParseStatus::PARSE_EXPECT_VALUE;  // 字符串结尾
             default:    return JsonParseStatus::PARSE_INVALID_VALUE;
         }
